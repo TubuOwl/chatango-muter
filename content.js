d94e6616fe8c58bfabfeb10112b12f217f6822b5
Tubu <74395702+TubuOwl@users.noreply.github.com>
2025-07-28 01:04:43 +0700

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,7 @@
+/* Rules #3 (epic.chatango.com): No harassment, you dont have to like everyone, just ignore them if you dont want to talk to them.
+
+   with full awareness I deleted them */
+
 const blockedUsers = ["loly"]; //throw a pie in his face if you meet him 
 
 function hideUserMessages() {
